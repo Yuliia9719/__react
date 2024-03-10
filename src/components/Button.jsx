8e51c77b1fe5fbb77f97ccda85e02334f9aabaec
Yuliia <iyulia1997len@gmail.com>
2024-03-10 13:33:53 +0200

    changed the file name

---
+++ src/components/Button.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+const Button = ({ initialButtonText, initialButtonColor, disabled }) => {
+  const [buttonText, setButtonText] = useState(initialButtonText);
+  const [buttonColor, setButtonColor] = useState(initialButtonColor);
+
+  const onClickHandler = () => {
+    setButtonText("You clicked me!"), setButtonColor("btn-info");
+  };
+  return (
+    <div>
+      <button
+        type="button"
+        disabled={disabled}
+        onClick={onClickHandler}
+        className={buttonColor}
+      >
+        {buttonText}
+      </button>
+      <h2>Button</h2>
+    </div>
+  );
+};
+
+export default Button;
